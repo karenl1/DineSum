874671702fbd9cbee1f2678c91a34e78e39272bd
uday <ualla@g.ucla.edu>
2017-11-07 15:47:21 -0800

    added getters/setters/constructor for RequestData Class

--- app/src/main/java/com/a0xffffffff/dinesum/RequestData.java
+++ app/src/main/java/com/a0xffffffff/dinesum/RequestData.java
@@ -13,4 +13,76 @@ public class RequestData {
     private double mPayment;
     private Date mRequestCreateTime;
 
+    public RequestData() {
+    }
+
+    public RequestData(Date startTime, Date endTime, String partyName, int numParty,
+                       Restaurant restaurant, double payment, Date requestCreateTime) {
+
+        this.mStartTime = startTime;
+        this.mEndTime = endTime;
+        this.mPartyName = partyName;
+        this.mNumParty = numParty;
+        this.mRestaurant = restaurant;
+        this.mPayment = payment;
+        this.mRequestCreateTime = requestCreateTime;
+
+    }
+
+    public Date getmStartTime() {
+        return mStartTime;
+    }
+
+    public void setmStartTime(Date mStartTime) {
+        this.mStartTime = mStartTime;
+    }
+
+    public Date getmEndTime() {
+        return mEndTime;
+    }
+
+    public void setmEndTime(Date mEndTime) {
+        this.mEndTime = mEndTime;
+    }
+
+    public double getmPayment() {
+        return mPayment;
+    }
+
+    public void setmPayment(double mPayment) {
+        this.mPayment = mPayment;
+    }
+
+    public String getmPartyName() {
+        return mPartyName;
+    }
+
+    public void setmPartyName(String mPartyName) {
+        this.mPartyName = mPartyName;
+    }
+
+    public Date getmRequestCreateTime() {
+        return mRequestCreateTime;
+    }
+
+    public void setmRequestCreateTime(Date mRequestCreateTime) {
+        this.mRequestCreateTime = mRequestCreateTime;
+    }
+
+    public void setmRestaurant(Restaurant mRestaurant) {
+        this.mRestaurant = mRestaurant;
+    }
+
+    public Restaurant getmRestaurant() {
+        return mRestaurant;
+    }
+
+    public int getmNumParty() {
+        return mNumParty;
+    }
+
+    public void setmNumParty(int mNumParty) {
+        this.mNumParty = mNumParty;
+    }
+
 }
