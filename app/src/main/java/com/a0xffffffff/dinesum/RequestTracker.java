27ab2841ce9449cefccdc07cb3051f7e73f25c28
Karen Li <karenli.614@gmail.com>
2017-11-08 21:56:04 -0800

    Change user to userID for requests

--- app/src/main/java/com/a0xffffffff/dinesum/RequestTracker.java
+++ app/src/main/java/com/a0xffffffff/dinesum/RequestTracker.java
@@ -19,7 +19,13 @@ public class RequestTracker {
     }
 
     public void filterNearbyRequests(Request[] allRequests) {
-
+        Request[] nearbyRequests = new Request[0];
+        for (Request request: allRequests) {
+            // check if request is in the same city as the user
+//            if (request.getRequestData().getmRestaurant().getRestaurantCity()) {
+//                // TODO
+//            }
+        }
     }
 
     private void setNearbyRequests(Request[] nearbyRequests) {
