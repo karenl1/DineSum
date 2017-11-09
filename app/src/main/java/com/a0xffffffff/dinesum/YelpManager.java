4853e1873d03088891c0c90b5885d1f47906df5c
Matthew Wong <mattwong949@gmail.com>
2017-11-08 18:40:45 -0800

    first commit test
    testing

--- app/src/main/java/com/a0xffffffff/dinesum/YelpManager.java
+++ app/src/main/java/com/a0xffffffff/dinesum/YelpManager.java
@@ -1,4 +1,31 @@
 package com.a0xffffffff.dinesum;
 
 public class YelpManager {
+
+    // members
+    private String grant_type;
+    private String client_id;
+    private String client_secret;
+
+    private String access_token;
+    private String token_type;
+    private int expires_in;
+
+    private String url_search = "GET https://api.yelp.com/v3/businesses/search";
+    private String url_
+
+    // public interface
+    public void autocomplete() {
+
+
+    }
+
+
+    public void getNearbyRestaurants() {
+
+
+    }
+
+
+
 }
