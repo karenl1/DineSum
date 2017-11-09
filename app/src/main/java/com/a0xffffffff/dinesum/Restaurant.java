576e53f7651e23cbbb7143377b4268052498dd42
uday <ualla@g.ucla.edu>
2017-11-08 21:19:05 -0800

    Restaurant getters/setters/constructors

--- app/src/main/java/com/a0xffffffff/dinesum/Restaurant.java
+++ app/src/main/java/com/a0xffffffff/dinesum/Restaurant.java
@@ -2,8 +2,41 @@ package com.a0xffffffff.dinesum;
 
 public class Restaurant {
     private String mRestaurantName;
+    private String mRestaurantAddress;
+    private String mRestaurantCity;
 
-    public Restaurant(String restaurantName) {
+    public Restaurant() {
+
+    }
+
+    public Restaurant(String restaurantName, String restaurantAddress, String restaurantCity) {
+        mRestaurantName = restaurantName;
+        mRestaurantAddress = restaurantAddress;
+        mRestaurantCity = restaurantCity;
+    }
+
+    public String getRestaurantName(){
+        return mRestaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName){
         mRestaurantName = restaurantName;
     }
+
+    public String getRestaurantAddress(){
+        return mRestaurantAddress;
+    }
+
+    public void setRestaurantAddress(String restaurantAddress){
+        mRestaurantAddress = restaurantAddress;
+    }
+
+    public String getRestaurantCity(){
+        return mRestaurantCity;
+    }
+
+    public void setRestaurantCity(String restaurantCity){
+        mRestaurantCity = restaurantCity;
+    }
+
 }
