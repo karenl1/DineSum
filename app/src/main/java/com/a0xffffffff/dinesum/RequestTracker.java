0b5d6c94ce2cc661ec925a6e8c3a540d6a6ae169
uday <ualla@g.ucla.edu>
2017-11-08 20:42:29 -0800

    RequestTracker and getNearbyRequest

--- app/src/main/java/com/a0xffffffff/dinesum/RequestTracker.java
+++ app/src/main/java/com/a0xffffffff/dinesum/RequestTracker.java
@@ -4,11 +4,18 @@ import java.util.Date;
 
 public class RequestTracker {
 
+    private static RequestTracker mRequestTracker = new RequestTracker();
+
     private User mCurrentUser;
     private Request[] mNearbyRequests;
 
     private RequestTracker() {
+        mRequestTracker = RequestTracker.getInstance();
+        
+    }
 
+    public static RequestTracker getInstance() {
+        return mRequestTracker;
     }
 
 }
