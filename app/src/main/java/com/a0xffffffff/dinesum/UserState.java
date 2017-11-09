3569847c665bd9f761ec6f959afbec534d515f7e
Matthew Wong <mattwong949@gmail.com>
2017-11-08 19:28:04 -0800

    added constructors to user_state
    added constructors

--- app/src/main/java/com/a0xffffffff/dinesum/UserState.java
+++ app/src/main/java/com/a0xffffffff/dinesum/UserState.java
@@ -5,6 +5,11 @@ public abstract class UserState {
 
     private User mUser;
 
+    public UserState(User new_user)
+    {
+    	mUser = new_user;
+    }
+
     public User getUser()
     {
         return mUser;
--- app/src/main/java/com/a0xffffffff/dinesum/UserStateRequester.java
+++ app/src/main/java/com/a0xffffffff/dinesum/UserStateRequester.java
@@ -2,6 +2,14 @@ package com.a0xffffffff.dinesum;
 
 public class UserStateRequester extends UserState {
 
+
+
+    public UserStateRequester(User new_user)
+    {
+        super(new_user);
+    }
+
+
     //To Implement
     public boolean makeRequest(Request request)
     {
