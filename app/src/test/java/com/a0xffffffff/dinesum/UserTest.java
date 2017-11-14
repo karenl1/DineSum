970b32f0efd6293912de7c1459058cefa1224f48
Matthew Wong <mattwong949@gmail.com>
2017-11-14 13:21:08 -0800

    created Testing Branch
    working on UserTest for now

---
+++ app/src/test/java/com/a0xffffffff/dinesum/UserTest.java
@@ -0,0 +1,19 @@
+package com.a0xffffffff.dinesum;
+
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class UserTest {
+
+    @Test
+    public void userCreatedSuccessfullyy(){
+        User testUser = new User();
+        RequestData testRequestData = new RequestData();
+        Request testRequest = new Request(testUser, testRequestData);
+    }
+
+
+
+}
