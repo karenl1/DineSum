67e17ab900baa20d5bb8afb129952d4a7e13e25f
Susan Krkasharian <skrkasharian@yahoo.com>
2017-11-15 13:50:27 -0800

    Add Request and User tests.

--- app/src/test/java/com/a0xffffffff/dinesum/RequestTest.java
+++ app/src/test/java/com/a0xffffffff/dinesum/RequestTest.java
@@ -7,12 +7,81 @@ import static org.junit.Assert.assertTrue;
 
 public class RequestTest {
 
+    private String requesterID = "requesterID";
+    private RequestData requestData = new RequestData("11:00", "12:00", "party",
+            2, "restaurant", 2.50);
+    private String requestID = "request";
+
+
 
     @Test
-    public void userCreatedSuccessfully(){
+    public void requestCreatedSuccessfully() {
+        Request request1 = new Request(requesterID, requestData, requestID);
 
+        System.out.println("Checking that request1 is not null");
+        assertTrue(request1 != null);
     }
 
+    @Test
+    public void testRequestID() {
+        Request request1 = new Request(requesterID, requestData, requestID);
 
+        System.out.println("Checking initial requestID value");
+        assertTrue(request1.getRequestID() == "request");
+    }
+
+    @Test
+    public void testRequesterID() {
+        Request request1 = new Request(requesterID, requestData, requestID);
+
+        System.out.println("Checking initial requesterID value");
+        assertTrue(request1.getRequesterID() == "requesterID");
+
+        String newRequesterID = "newRequesterID";
+        System.out.println("Setting requesterID to new value");
+        request1.setRequesterID(newRequesterID);
+        assertTrue(request1.getRequesterID() == "newRequesterID");
+    }
+
+    @Test
+    public void testReserverID() {
+        Request request1 = new Request(requesterID, requestData, requestID);
+
+        System.out.println("Checking initial reserverID value");
+        assertTrue(request1.getReserverID() == null);
+
+        String newReserverID = "newReserverID";
+        System.out.println("Setting reserverID to new value");
+        request1.setReserverID(newReserverID);
+        assertTrue(request1.getReserverID() == "newReserverID");
+    }
+
+    @Test
+    public void testRequestState() {
+        Request request1 = new Request(requesterID, requestData, requestID);
+
+        System.out.println("Checking initial requestState value (PENDING)");
+        assertTrue(request1.getRequestState() == RequestState.PENDING);
+
+        System.out.println("Setting requestState to next state (CLAIMED)");
+        request1.setRequestState(RequestState.CLAIMED);
+        assertTrue(request1.getRequestState() == RequestState.CLAIMED);
+
+        System.out.println("Setting requestState to next state (COMPLETED)");
+        request1.setRequestState(RequestState.COMPLETED);
+        assertTrue(request1.getRequestState() == RequestState.COMPLETED);
+
+        System.out.println("Setting requestState to next state (PAID)");
+        request1.setRequestState(RequestState.PAID);
+        assertTrue(request1.getRequestState() == RequestState.PAID);
+    }
+
+    // requestID - DONE
+    // requesterID - DONE
+    // reserverID - DONE
+    // requestState - DONE
+
+
+    // requestData
 
 }
--- app/src/test/java/com/a0xffffffff/dinesum/UserTest.java
+++ app/src/test/java/com/a0xffffffff/dinesum/UserTest.java
@@ -10,35 +10,84 @@ public class UserTest {
     private String requesterID1 = "requester1";
     private RequestData requestData1 = new RequestData("11:00", "12:00", "party1",
             2, "restaurant1", 2.50);
-    Request request1 = new Request(requesterID1, requestData1);
+    private String requestID1 = "request1";
+    Request request1 = new Request(requesterID1, requestData1, requestID1);
 
     private String requesterID2 = "requester2";
     private RequestData requestData2 = new RequestData("2:00", "3:00", "party2",
             4, "restaurant2", 3.00);
-    Request request2 = new Request(requesterID2, requestData2);
+    private String requestID2 = "request2";
+    Request request2 = new Request(requesterID2, requestData2, requestID2);
 
+    /*
+    protected void setUp(){
+        System.out.println("Set up");
+    }
+    */
 
     @Test
     public void userCreatedSuccessfully(){
         User testUser = new User();
 
+        System.out.println("Checking to make sure Lists are not null and empty");
         assertTrue(testUser.getRequests() != null && testUser.getRequests().isEmpty());
         assertTrue(testUser.getReservations() != null && testUser.getReservations().isEmpty());
     }
 
     @Test
     public void requestAddedRemovedSuccessfully() {
+        User testUser = new User();
 
+        System.out.println("Adding Request 1 to mRequests");
+        assertTrue(testUser.addRequest(request1));
+        assertTrue(testUser.getRequests().size() == 1);
+        assertTrue(testUser.getRequests().get(0) == request1);
+
+        System.out.println("Adding Request 2 to mRequests");
+        assertTrue(testUser.addRequest(request2));
+        assertTrue(testUser.getRequests().size() == 2);
+        assertTrue(testUser.getRequests().get(0) == request1);
+        assertTrue(testUser.getRequests().get(1) == request2);
+
+        System.out.println("Removing Request 1 from mRequests");
+        assertTrue(testUser.removeRequest(request1));
+        assertTrue(testUser.getRequests().size() == 1);
+        assertTrue(testUser.getRequests().get(0) == request2);
+
+        System.out.println("Removing Request 2 from mRequests");
+        assertTrue(testUser.removeRequest(request2));
+        assertTrue(testUser.getRequests().size() == 0);
     }
 
     @Test
     public void reservationAddedRemovedSuccessfully() {
+        User testUser = new User();
 
+        System.out.println("Adding Request 1 to mReservations");
+        assertTrue(testUser.addReservation(request1));
+        assertTrue(testUser.getReservations().size() == 1);
+        assertTrue(testUser.getReservations().get(0) == request1);
+
+        System.out.println("Adding Request 2 to mReservations");
+        assertTrue(testUser.addReservation(request2));
+        assertTrue(testUser.getReservations().size() == 2);
+        assertTrue(testUser.getReservations().get(0) == request1);
+        assertTrue(testUser.getReservations().get(1) == request2);
+
+        System.out.println("Removing Request 1 from mReservations");
+        assertTrue(testUser.removeReservation(request1));
+        assertTrue(testUser.getReservations().size() == 1);
+        assertTrue(testUser.getReservations().get(0) == request2);
+
+        System.out.println("Removing Request 2 from mReservations");
+        assertTrue(testUser.removeReservation(request2));
+        assertTrue(testUser.getReservations().size() == 0);
     }
 
+    /*
     @Test
     public void facebookIDSuccessful() {
-        
-    }
 
+    }
+    */
 }
