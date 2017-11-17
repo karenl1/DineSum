7c5e0aeb15a33ebf5c7748046572da188cf29590
Susan Krkasharian <skrkasharian@yahoo.com>
2017-11-16 19:13:03 -0800

    Add testUserWriteRequestSuccessfully.

--- app/src/test/java/com/a0xffffffff/dinesum/FirebaseManagerTest.java
+++ app/src/test/java/com/a0xffffffff/dinesum/FirebaseManagerTest.java
@@ -1,17 +1,51 @@
 package com.a0xffffffff.dinesum;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class FirebaseManagerTest {
+
+    /**
+     * This test checks if a request created by the user is successfully written to Firebase and
+     * if the event listener for the requests for the current user correctly returns the list of
+     * this user's created requests.
+     */
     @Test
-    public void writesRequestSuccessfully(){
-        String userID = "1743480282342335";
-        RequestData testRequestData = new RequestData();
-        FirebaseManager firebaseManager = FirebaseManager.getInstance();
+    public void testUserWriteRequestSuccessfully(){
+        // create dummy request object needed for test
+        String userID = "111";
+        Restaurant testRestaurant = new Restaurant("id", "name", "address", "phoneNum",
+                "city");
+        RequestData testRequestData = new RequestData("startTime", "endTime", "partyName",
+                4, testRestaurant, 1.0);
         Request testRequest = new Request(userID, testRequestData);
-        firebaseManager.writeRequest(testRequest);
+        // add event listener to Firebase to detect new requests created by the current user
+        DatabaseReference requestDatabase = FirebaseManager.getInstance().getRequestDatabase();
+        requestDatabase.orderByChild("requesterID").equalTo(userID)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) { //something changed!
+                        for (DataSnapshot requestSnapshot : dataSnapshot.getChildren()) {
+                            Request newRequest = FirebaseManager.parseJson(requestSnapshot);
+                            Log.d("Test FirebaseManager user request", "requesterID" + newRequest
+                                    .getRequesterID
+                                            ());
+                            // check if user requests have correct userID
+                            assertTrue(String.equals(newRequest.getRequesterID(),userID));
+                        }
+                    }
+                });
+        // write requests to Firebase
+        FirebaseManager.getInstance().writeRequest(testRequest);
     }
-}
