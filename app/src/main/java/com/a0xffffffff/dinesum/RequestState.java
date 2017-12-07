2a6a887580b2991fec1a8dd8cd903f1856567856
Stanley Hsu <sths0614@ucla.edu>
2017-12-07 04:32:40 -0800

    capitalize request states

--- app/src/main/java/com/a0xffffffff/dinesum/RequestState.java
+++ app/src/main/java/com/a0xffffffff/dinesum/RequestState.java
@@ -3,9 +3,9 @@ package com.a0xffffffff.dinesum;
 public class RequestState {
 
     // not using enum because hard to use with Firebase
-    public static final String PENDING = "pending";
-    public static final String CLAIMED = "claimed";
-    public static final String COMPLETED = "completed";
-    public static final String PAID = "paid";
+    public static final String PENDING = "Pending";
+    public static final String CLAIMED = "Claimed";
+    public static final String COMPLETED = "Completed";
+    public static final String PAID = "Paid";
 
 }
