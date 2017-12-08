d513110fa170bef4abcf7f3dbcb28b62a2a45022
Susan Krkasharian <skrkasharian@yahoo.com>
2017-12-08 13:56:58 -0800

    Add manual test_requests, read in json file into JSONArray

--- app/src/test/java/com/a0xffffffff/dinesum/FirebaseManagerTest.java
+++ app/src/test/java/com/a0xffffffff/dinesum/FirebaseManagerTest.java
@@ -10,85 +10,83 @@ import com.google.firebase.database.ValueEventListener;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class FirebaseManagerTest {
+    JSONParser parser = new JSONParser();
+    JSONArray jsonFile;
 
-    /**
-     * This test checks if a request created by the user is successfully written to Firebase and
-     * if the event listener for the requests for the current user correctly returns the list of
-     * this user's created requests.
-     */
-    @Test
-    public void testUserWriteRequestSuccessfully() {
-        // create dummy request object needed for test
-        String userID = "111";
-        Restaurant testRestaurant = new Restaurant("id", "name", "address", "phoneNum",
-                "city");
-        RequestData testRequestData = new RequestData("startTime", "endTime", "partyName",
-                4, testRestaurant, 1.0);
-        Request testRequest = new Request(userID, testRequestData);
-        // add event listener to Firebase to detect new requests created by the current user
-        DatabaseReference requestDatabase = FirebaseManager.getInstance().getRequestDatabase();
-        requestDatabase.orderByChild("requesterID").equalTo(userID)
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) { //something changed!
-                        for (DataSnapshot requestSnapshot : dataSnapshot.getChildren()) {
-                            Request newRequest = FirebaseManager.parseJson(requestSnapshot);
-                            Log.d("Test FirebaseManager user request", "requesterID" + newRequest
-                                    .getRequesterID
-                                            ());
-                            // check if user requests have correct userID
-                            assertTrue(String.equals(newRequest.getRequesterID(), userID));
-                        }
-                    }
-                });
-        // write requests to Firebase
-        FirebaseManager.getInstance().writeRequest(testRequest);
+    public JSONArray file_to_json() throws IOException, ParseException
+    {
+        jsonFile = (JSONArray) parser.parse(new FileReader("dinesum-requests-export.json"));
+        return jsonFile;
     }
+    public Request[] create_test_requests()
+    {
+        //Request1
+        String requesterID = "10215291050977832";
+        Restaurant restaurant = new Restaurant("ChIJ__Jsmgq7woARhmKIwcjfc0o", "Popcorn Chicken Taste of Taiwan",
+                "+1 424-832-3076", "2224 Sawtelle Blvd, Los Angeles, CA 90064, USA", "Los Angeles");
+        RequestData requestData = new RequestData("4:31", "5:1", "Stan",
+                3, restaurant, 6);
+        String requestID = "-L-kxRDFoFoSovybwWbd";
 
-    /**
-     * This test checks if a nearby request (in the same city as the user) is successfully written
-     * to Firebase and if the event listener for the nearby requests correctly returns this user's
-     * nearby requests.
-     */
-    @Test
-    public void testNearbyWriteRequestSuccessfully() {
-        // create dummy request object needed for test
-        String userID = "222";
-        String userCity = "Los Angeles";
-        Restaurant testRestaurant = new Restaurant("id", "name", "address", "phoneNum",
-                "Los Angeles");
-        RequestData testRequestData = new RequestData("startTime", "endTime", "partyName",
-                4, testRestaurant, 1.0);
-        Request testRequest = new Request(userID, testRequestData);
-        // add event listener to Firebase to detect new nearby requests
-        DatabaseReference requestDatabase = FirebaseManager.getInstance().getRequestDatabase();
-        requestDatabase.orderByChild("requestData/restaurant/restaurantCity").equalTo(userCity)
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) { //something changed!
-                        for (DataSnapshot requestSnapshot : dataSnapshot.getChildren()) {
-                            Request newRequest = FirebaseManager.parseJson(requestSnapshot);
-                            Log.d("Test FirebaseManager nearby request", "requestCity" +
-                                    newRequest.getRequestData().getRestaurant().getRestaurantCity());
-                            // check if nearby requests have correct city
-                            assertTrue(String.equals(newRequest.getRequestData().getRestaurant()
-                                    .getRestaurantCity(), userCity));
-                        }
-                    }
-                });
-        // write requests to Firebase
-        FirebaseManager.getInstance().writeRequest(testRequest);
+        Request request1 = new Request(requesterID, requestData, requestID);
+
+        //Request2
+        requesterID = "10215291050977832";
+        restaurant = new Restaurant("ChIJ4zrX7Ii8woARDS6Jslp0j3Y", "Yoshinoya UCLA",
+                "+1 310-443-3681", "617 Charles E Young Dr E, Los Angeles, CA 90095, USA", "Los Angeles");
+        requestData = new RequestData("8:31", "9:07", "Dude",
+                2, restaurant, 5);
+        requestID = "-L-kxZPJaG9yq5JPbdNo";
+
+        Request request2 = new Request(requesterID, requestData, requestID);
+
+        //Request3
+        requesterID = "1758223867534643";
+        restaurant = new Restaurant("ChIJqc1-Q5u4woARAzOO1E824cY", "Hae Jang Chon Korean BBQ Restaurant",
+                "+1 213-389-8777", "3821 W 6th St, Los Angeles, CA 90020, USA", "Los Angeles");
+        requestData = new RequestData("20:35", "23:42", "Daisy",
+                4, restaurant, 10);
+        requestID = "-L-mOdIwjUINbLReWCDy";
+
+        Request request3 = new Request(requesterID, requestData, requestID);
+
+        //Request4
+        requesterID = "1499052203496028";
+        restaurant = new Restaurant("ChIJ9TQdLHW7woARYBaUqZ2X6N8", "Tatsu Ramen",
+                "+1 310-684-2889", "2123 Sawtelle Blvd, Los Angeles, CA 90025, USA", "Los Angeles");
+        requestData = new RequestData("19:6", "20:36", "KAREN",
+                6, restaurant, 10000000);
+        requestID = "-L-nDzF3z1canFyiBzBl";
+
+        Request request4 = new Request(requesterID, requestData, requestID);
+
+        Request[] test_requests = new Request[]{request1, request2, request3, request4};
+
+        return test_requests;
     }
-    /**
-     * Check if Firebase returns string ID for requests
-     */
     @Test
-    public void testFirebaseKeyGeneration() {
-        assertTrue(FirebaseManager.getInstance().getNewRequestID() instanceof String);
+    public Request test_parseJson() throws IOException, ParseException
+    {
+        JSONArray jsonFile = file_to_json();
+        for (Object o : jsonFile)
+        {
+
+        }
+        System.out.println("Checking that request1 is not null");
+        assertTrue(request1 != null);
     }
 }
