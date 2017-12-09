50151af45e2b734fa74f4f97895af95cac68b252
Matthew Wong <mattwong949@gmail.com>
2017-12-08 18:32:47 -0800

    attempting to add user-rating feature
    still need to initially add user to the firebase database the first
    time the user logs in

---
+++ app/src/main/java/com/a0xffffffff/dinesum/UserTracker.java
@@ -0,0 +1,36 @@
+package com.a0xffffffff.dinesum;
+
+import java.util.ArrayList;
+
+
+/**
+ * Stores list of users received from the FirebaseManager
+ * keeps track of ratings for users
+ */
+public class UserTracker {
+
+    private static UserTracker mUserTracker = new UserTracker();
+
+    private ArrayList<User> mAllUsers;
+
+    private UserTracker() {
+    }
+
+    public static UserTracker getInstance() {
+        return mUserTracker;
+    }
+
+    public void setAllUsers(ArrayList<User> allUsers) { mAllUsers = allUsers; }
+
+    public ArrayList<User> getAllUsers() { return mAllUsers; }
+
+    public double getUserPointsFromDatabase(String userID) {
+        for (User u : mAllUsers) {
+            if (u.getUserID() == userID)
+                return u.getPoints();
+        }
+        return -1;
+    }
+
+
+}
